12f60e12329f285020d0ac033a2e03922cbf444b
Avirek Ghatia <avirek.ghatia@meraki.net>
2020-05-12 14:01:42 -0700

    build.groovy

---
+++ build.groovy
@@ -0,0 +1,6 @@
+def entry_point() {
+    withCredentials([usernameColonPassword('jenkins-avirek-creds', variable: 'CREDENTIALS')]) {
+        sh "echo $CREDENTIALS"
+        sh "curl -u $CREDENTIALS meraki.com"
+    }
+}
