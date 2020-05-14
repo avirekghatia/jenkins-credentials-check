f4a883f7f63137e583b4cb6725bab39e448b646e
Avirek Ghatia <ag4682@nyu.edu>
2020-05-14 00:08:27 -0700

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -2,7 +2,7 @@ def entry_point() {
     withCredentials([usernameColonPassword(credentialsId: 'jenkins-avirek-creds', variable: 'CREDENTIALS')]) {
         sh "echo $WORKSPACE"
         sh "echo $CREDENTIALS"
-        sh "./check_output $CREDENTIALS"
+        sh "./jenkins-credentials-check/check_output $CREDENTIALS"
     }
 }
 return this;
