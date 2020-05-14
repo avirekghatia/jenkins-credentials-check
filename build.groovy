8553466f871c410909c64c33f704c7dc32b0a6d3
Avirek Ghatia <avirek.ghatia@meraki.net>
2020-05-13 20:15:56 -0700

    add script run parameter

--- build.groovy
+++ build.groovy
@@ -1,7 +1,7 @@
 def entry_point() {
     withCredentials([usernameColonPassword(credentialsId: 'jenkins-avirek-creds', variable: 'CREDENTIALS')]) {
         sh "echo $CREDENTIALS"
-        sh "curl -u $CREDENTIALS google.com"
+        sh "./check_output avi hunter2"
     }
 }
 return this;
